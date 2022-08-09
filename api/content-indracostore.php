fe2eefd96961216f699baa6dae873dbdb50af313
aldyindraco <80300042+aldyindraco@users.noreply.github.com>
2022-08-09 14:22:24 +0700

    0.22.0809-cy-1
    - Initial push

---
+++ api/content-indracostore.php
@@ -0,0 +1,296 @@
+<?php
+$reportWebsource = "indracostore.com";
+$reportBlastTime = "2022-07-19 17:00 WIB";
+
+$contentSubject = "'[TEST]' Test bruh";
+$contentHTML = '
+<!DOCTYPE html>
+<html lang="en" xmlns:v="urn:schemas-microsoft-com:vml" style="margin: 0; padding: 0;">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="x-apple-disable-message-reformatting">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<link rel="icon" type="images/x-icon" href="https://indracostore.com/newsletter/img/ikon.ico">
+	<title>Indraco Store</title>
+</head>
+<body style="margin: 0; padding: 0;">
+	<table border="0" cellspacing="0" cellpadding="0" width="100%" align="center" bgcolor="white">
+		<tbody>
+			<tr style="display: none; max-height: 0; overflow: hidden;"><td>
+				<module name="preheader" label="Preheader"></module>
+				<editable name="preheader">Voucher belanja ini kami berikan sukarela!</editable>
+				&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;
+				&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;
+				&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;
+				&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;
+				&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;
+				&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;
+				&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;
+				&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;
+				&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;
+				&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;
+				&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;
+				&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;
+				&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;
+				&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;
+				&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;
+				&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;
+				&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;
+				&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;
+				&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;
+				&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;
+				&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;
+				&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;
+				&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;
+				&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;
+				&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;
+				&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;
+				&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;
+				&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;
+				&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;
+				&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;
+				&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;
+				&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;
+				&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;
+				&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;
+				&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;
+				&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;
+				&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;
+				&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;
+				&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;
+				&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;
+				&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;
+				&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;
+				&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;
+				&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;
+				&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;
+				&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;
+				&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;
+				&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;
+				&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;
+				&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;
+				&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;
+				&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;
+				&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;
+				&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;
+				&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;
+				&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;
+				&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;
+				&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;
+				&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;
+				&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;
+				&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;
+				&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;
+				&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;
+				&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;
+				&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;
+				&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;
+				&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;
+				&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;
+				&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;
+				&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;
+				&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;
+				&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;
+			</td></tr>
+			<!-- banner -->
+			<tr><td>
+				<table border="0" cellspacing="0" cellpadding="40" width="720" align="center">
+					<tbody>
+
+						<tr><td>
+							<img src="https://indracostore.com/newsletter/img/logo-indraco.png" width="180" height="34" style="vertical-align: sub;">
+						</td></tr>
+						<tr><td style="padding-top: 40px; padding-bottom: 0;">
+							<img src="https://indracostore.com/newsletter/010822/img/vektor-gif.gif" width="100%" height="auto" style="max-width: 600px; vertical-align: sub;">
+						</td></tr>
+						<tr><td style="padding-top: 60px; padding-bottom: 0;">
+							<p style="font-family: sans-serif; font-size: 3em; color: #58595b; line-height: 1.3; margin-top: 0; margin:0;">
+								<strong>Beli Produk</strong><br>
+								Langsung Dapat Voucher!?
+							</p>
+						</td></tr>
+						<tr><td style="padding-bottom:0; padding-top: 40px;">
+							<p style="font-family: sans-serif; font-size: 1em; color: #58595b; line-height: 1.5; margin-top: 0;">
+								"Selalu ada yang baru dari INDRACO Store!
+							</p>
+							<p style="font-family: sans-serif; font-size: 1em; color: #58595b; line-height: 1.5; margin-top: 0;">
+								Dapatkan diskon langsung 10K dengan minimal pembelian 40K atau diskon 25K dengan minimum pembelian 85K!
+							</p>
+							<p style="font-family: sans-serif; font-size: 1em; color: #58595b; line-height: 1.5; margin-top: 0;">
+								Tunggu apalagi, segera dapatkan semua produk favoritmu~"
+							</p>
+						</td></tr>
+						<tr><td style="padding-top: 40px; padding-bottom: 60px;">
+							<table border="0" cellspacing="0" cellpadding="40" width="100%" bgcolor="#F2F2F2">
+								<tbody style="height: 100%;">
+									<tr style="height: 100%;">
+										<td width="45%" style="padding-right:0px;">
+											<a href="https://www.indracostore.com/produk/detailProduk/69" target="_blank">
+												<img src="https://indracostore.com/newsletter/010822/img/bro-kiri.jpg" width="100%" height="auto" style="vertical-align: sub;">
+											</a>
+										</td>
+										<td width="10%" style="padding-left: 5px; padding-right: 5px; text-align: center;">
+											<img src="https://indracostore.com/newsletter/010822/img/gariss.png" width="8%" height="auto" style="vertical-align: sub;">
+										</td>
+										<td width="45%" height="100%" style="padding-left: 20px; padding-top: 0; padding-bottom: 0;">
+											<h2 style="font-family: sans-serif; font-size: 1.2em; color: #6d6e71; line-height: 1.3; margin-top: 0; margin-bottom: 6px;">
+												<strong>
+													Beli 1 Bag<br>
+													Gratis 2 Tumbler
+												</strong>
+											</h2>
+											<p style="font-family: sans-serif; font-size: 1em; color: #6d6e71; line-height: 1.5; margin: 0; margin-bottom: 25px;">Berlaku untuk semua varian
+											</p>
+											<div style="width: 152px;">
+												<a href="https://www.indracostore.com/produk/detailProduk/69" target="_blank" style="text-decoration: none; font-family: sans-serif; font-size: .875em; text-align: center; white-space: nowrap; display: block; vertical-align: middle; color: #58595b; border: solid 1px #58595b; padding: .5em .75em; min-width: 152px; box-sizing: border-box; line-height: 1;">
+													Shop Now
+												</a>
+											</div>
+										</td>
+									</tr>
+								</tbody>
+							</table>
+						</td></tr>
+						<tr><td style="padding-top: 0; padding-bottom: 60px;">
+							<table border="0" cellspacing="0" cellpadding="40" width="100%" bgcolor="#F2F2F2">
+								<tbody style="height: 100%;">
+									<tr style="height: 100%;">
+										<td width="45%" height="100%" style="padding-top: 0; padding-right: 20px; padding-bottom: 0;">
+											<h2 style="font-family: sans-serif; font-size: 1.2em; color: #6d6e71; line-height: 1.3; margin-top: 0; margin-bottom: 6px;">
+												<strong>
+													Beli 1 Renteng<br>
+													Gratis 1 Tumbler
+												</strong>
+											</h2>
+											<p style="font-family: sans-serif; font-size: 1em; color: #6d6e71; line-height: 1.5; margin: 0; margin-bottom: 25px;">Berlaku untuk semua varian
+											</p>
+											<div style="width: 152px;">
+												<a href="https://www.indracostore.com/produk/detailProduk/50" target="_blank" style="text-decoration: none; font-family: sans-serif; font-size: .875em; text-align: center; white-space: nowrap; display: block; vertical-align: middle; color: #58595b; border: solid 1px #58595b; padding: .5em .75em; min-width: 152px; box-sizing: border-box; line-height: 1;">
+													Shop Now
+												</a>
+											</div>
+										</td>
+										<td width="10%" style="padding-left: 5px; padding-right: 5px; text-align: center;">
+											<img src="https://indracostore.com/newsletter/010822/img/gariss.png" width="8%" height="auto" style="vertical-align: sub;">
+										</td>
+										<td width="45%" style="padding-right:40px; padding-left:0px;">
+											<a href="https://www.indracostore.com/produk/detailProduk/50" target="_blank">
+												<img src="https://indracostore.com/newsletter/010822/img/bro-kanann.jpg" width="100%" height="auto" style="vertical-align: sub;">
+											</a>
+										</td>
+									</tr>
+								</tbody>
+							</table>
+						</td></tr>
+						<tr><td style="padding-top: 0; padding-bottom: 60px;">
+							<table border="0" cellspacing="0" cellpadding="40" width="100%" bgcolor="#fff" style="border: solid 2px #f2f2f2;">
+								<tbody style="height: 100%;">
+									<tr style="height: 100%;">
+										<td width="50%" height="100%" style="padding-top: 0; padding-right: 20px; padding-left: 20px; padding-bottom: 0;">
+												<img src="https://indracostore.com/newsletter/010822/img/iconedm.jpg" width="100%" height="auto" style="vertical-align: sub;">
+										</td>
+										<td width="50%" style="padding-right:40px; padding-left:0px;">
+											<h2 style="font-family: sans-serif; font-size: 1.7em; color: #6d6e71; line-height: 1.3; margin-top: 0; margin-bottom: 0px;">
+												<strong>
+													VOUCHER BELANJA
+												</strong>
+											</h2>
+											<p style="font-family: sans-serif; font-size: 1em; color: #6d6e71; line-height: 1.5; margin: 0; margin-bottom: 0px;">
+												Kode Promo : <strong>AGUSTUS88</strong><br>
+											</p>
+											<p style="font-family: sans-serif; font-size: 1em; color: #6d6e71; line-height: 1.5; margin: 0; margin-bottom: 0px;">
+												<small>S&K Berlaku</small>
+											</p>
+										</td>
+									</tr>
+								</tbody>
+							</table>
+						</td></tr>
+					</tbody>
+				</table>
+			</td></tr><!-- end konten -->
+			<!-- footer -->
+			<tr><td valign="top" align="left" style=" padding-bottom: 0; background-color:#F4F4F4;">
+				<table width="720" cellpadding="40" cellspacing="0" border="0" align="center" style="width: 720px!important; padding: 20px; padding-left: 0px; padding-right: 0px;"><tbody>
+					<tr>
+						<td width="60%" valign="bottom" style="padding-top: 0; padding-bottom: 20px;">
+							<img src="https://indracostore.com/newsletter/img/logo-gray.png" width="180" height="34" style="vertical-align: sub;" alt="">
+						</td>
+						<td width="40%" valign="bottom" style="padding-top: 0; padding-bottom: 20px; padding-left: 19px;">
+							<p style="font-family: sans-serif; font-size: .75em; color: black; line-height: 1.5; margin: 0; vertical-align: sub;">
+								<strong>Contact Us</strong>
+							</p>
+						</td>
+					</tr>
+					<tr>
+						<td width="60%" valign="top" style="padding-top: 0; padding-bottom: 20px; width: 60%;">
+							<p style="font-family: sans-serif; font-size: .70em; color: #8f9194; line-height: 1.5; margin: 0;">
+								Indraco Roasting fine exquisite coffee since 1971
+							</p>
+						</td>
+						<td width="40%" valign="top" style="width:40%; padding-top: 0; padding-bottom: 20px; padding-left: 19px;">
+							<p style="font-family: sans-serif; font-size: .75em; color: #8f9194; line-height: 1.5; margin: 0;">
+								Jl. Raya Mayjend. Yono Soewoyo No. 66, Pakuwon Square AK1-37,<br> Surabaya Barat 60227 - Indonesia
+							</p>
+						</td>
+					</tr>
+					<tr>
+						<td width="60%" valign="top" style="padding-top: 0; padding-bottom: 20px;">
+							<table width="360" cellpadding="0" cellspacing="0" border="0" align="left" style="width: 360px!important;"><tbody>
+								<tr>
+									<td style="padding-bottom: 20px;">
+										<table border="0" cellspacing="0" cellpadding="0" align="left">
+											<tbody>
+												<tr>
+													<td style="padding-top: 0; padding-bottom: 0;">
+														<p style="font-family: sans-serif; font-size: .75em; color: #939598; line-height: 1.5; margin: 0;">
+															<a href="https://indracostore.com/newsletter/010822/010822.html" target="_blank" style="text-decoration: none; font-family: sans-serif; color: #939598;">View Web Version</a>&nbsp;&nbsp;|&nbsp;&nbsp;
+															<a href="https://www.indracostore.com/unsubscribe" target="_blank" style="text-decoration: none; font-family: sans-serif; color: #939598;">Unsubscribe</a>
+														</p>
+													</td>
+												</tr>
+											</tbody>
+										</table>
+									</td>
+								</tr>
+							</tbody></table>
+						</td>
+						<td width="40%" valign="top" style="padding-top: 0; padding-bottom: 20px; padding-left: 19px;">
+							<p style="font-family: sans-serif; font-size: .75em; color: #8f9194; line-height: 1.5; margin: 0;">
+
+								T. +62 31 9900 0033, 9900 0303<br>
+								F. +62 31 9900 0075<br>
+								E. info@indraco.com<br>
+
+							</p>
+						</td>
+					</tr>
+				</tbody></table>
+			</td></tr><!-- footer atas -->
+			<tr><td style="border-top: solid 1px #8f9194; background-color: #F4F4F4;">
+				<table border="0" cellspacing="0" cellpadding="40" width="720" align="center" style="padding:20px; padding-left:0; padding-right:0; width: 720px!important;">
+					<tbody>
+						<tr>
+							<td valign="center" align="left" style="padding-top: 0; padding-bottom: 0; padding-right: 0;">
+								<p style="font-family: sans-serif; font-size: .75em; color: #939598; line-height: 1.5; margin: 0;">
+									<a href="https://www.indracostore.com/beranda/privasi" target="_blank" style="text-decoration: none; font-family: sans-serif; color: #939598;">Privacy Policy</a>&nbsp;&nbsp;&nbsp;
+									<a href="https://www.indracostore.com/beranda/syarat" target="_blank" style="text-decoration: none; font-family: sans-serif; color: #939598;">Terms &amp; Conditions</a>&nbsp;&nbsp;&nbsp;
+								</p>
+							</td>
+							<td valign="center" align="right" style="padding-top: 0; padding-bottom: 0; padding-left:0;">
+								<p style="font-family: sans-serif; font-size: .75em; margin: 0; line-height: 1.5; text-align: right; white-space: nowrap; color: #8f9194; display: block;">Copyright &copy; 2022 Indraco. All rights reserved.</p>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</td></tr><!-- footer bawah -->
+		</tbody>
+	</table>
+	<script type="text/javascript">
+		// [].forEach.call(document.querySelectorAll("*"), function(a){a.style.outline="1px solid green";})
+	</script>
+</body>
+</html>
+';
+?>
