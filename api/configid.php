970656bf80387af8ba6278697b66c9484e3c31d1
aldyindraco <80300042+aldyindraco@users.noreply.github.com>
2022-08-09 14:36:05 +0700

    0.22.0809-cy-2
    - add DB last

--- api/configid.php
+++ api/configid.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$database = "supresso_newsupressoid";
+$database = "supresso_mailblast";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $database);
